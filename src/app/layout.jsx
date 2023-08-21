555ea0043b7fa89c8d1ac1205c242a6e89937065
steven <steven69700@gmail.com>
2023-08-21 13:55:21 +0200

    finalisation des fonctionnalités du LP compter

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,7 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import { Header } from './component/layout/Header'
 
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
