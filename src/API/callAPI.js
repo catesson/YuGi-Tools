12ef413099595a320fa7b1af57bba2cff68d2907
steven <steven69700@gmail.com>
2023-09-11 13:43:58 +0200

    update form search

--- src/API/callAPI.js
+++ src/API/callAPI.js
@@ -4,44 +4,47 @@ import allCard from "../data/data.json";
 export async function getAllCard(params) {
   if (mocked) {
     const cards = allCard.map((card) => {
-      return card;
-    });
+      
+            return card;
+    })
+
     return cards;
+  } else {
+    const url = params
+      ? `http://localhost:3001/cards?${params.toString()}`
+      : "http://localhost:3001/cards";
+    const response = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    });
+    if (!response.ok) {
+      console.log("Mauvaise réponse du réseau");
+    } else {
+      //contient la liste de toutes les cartes et le nombre maximum de page {cards : [cards], maxPage : page}
+      const JsonResponse = await response.json();
+
+      return JsonResponse;
+    }
   }
-
-console.log(params)
-  const url = params ? `http://localhost:3001/cards?${params.toString()}` : "http://localhost:3001/cards"
-  const response = await fetch(url, {
-    
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-  }, )    
-      if (!response.ok) {
-        console.log("Mauvaise réponse du réseau");
-      } 
-      else {
-        //contient la liste de toutes les cartes et le nombre maximum de page {cards : [cards], maxPage : page}
-        const JsonResponse = await response.json()
-
-        return JsonResponse
-      }
 }
 export async function getCard(id) {
   if (mocked) {
     const card = allCard.find((card) => card.id == id);
     return card;
   }
-  const response = await fetch(`http://localhost:3001/cards/${id}`, {next: { revalidate: 40}}, {
-    
-    method: "GET",
-    headers: { "Content-Type": "application/json" },
-  }, )    
-      if (!response.ok) {s
-        console.log("Mauvaise réponse du réseau");
-      } 
-      else {
-        
-        const card = await response.json()
-        return card
-      }
+  const response = await fetch(
+    `http://localhost:3001/cards/${id}`,
+    { next: { revalidate: 40 } },
+    {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    }
+  );
+  if (!response.ok) {
+    s;
+    console.log("Mauvaise réponse du réseau");
+  } else {
+    const card = await response.json();
+    return card;
+  }
 }
