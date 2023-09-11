f8047893c651ced20ed2c2645b6d30b360bfcfa4
steven <steven69700@gmail.com>
2023-09-11 16:25:23 +0200

    automatisation de l'affichage des type et des attributs dans la recherche

--- src/API/callAPI.js
+++ src/API/callAPI.js
@@ -30,6 +30,23 @@ export async function getAllCard(params) {
     }
   }
 }
+
+export async function getMonsterFilter() {
+  const response = await fetch("http://localhost:3001/cards/monster_filter", {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  });
+  if (!response.ok) {
+    window.alert("Le server n'a pas retourner les Monster Filter");
+    return 
+  } else {
+    //contient la liste de toutes les cartes et le nombre maximum de page {cards : [cards], maxPage : page}
+    const JsonResponse = await response.json();
+  
+
+    return JsonResponse;
+  }
+}
 export async function getCard(id) {
   if (mocked) {
     const card = allCard.find((card) => card.id == id);
