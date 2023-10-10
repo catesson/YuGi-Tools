4f433bccccdd229e58b9fc6a82b59e9aef02f66b
steven <steven69700@gmail.com>
2023-10-10 17:56:01 +0200

    utilisation d'élément jsx pour les icon de profil plutot que des images

--- src/app/component/LittleCard.jsx
+++ src/app/component/LittleCard.jsx
@@ -1,10 +1,10 @@
-import Image from 'next/image'
+/* eslint-disable @next/next/no-img-element */
 export default function LittleCard({name, image, id}){
 
     return (
         <div className="w-52 mx-3">
             <a href={`/card/${id}`}>
-            <Image className="m-auto" style={{maxWidth:'168px', maxHeight:'246px'}} src={`${image.image_url_small}`} alt={name}/>
+            <img className="m-auto" style={{maxWidth:'168px', maxHeight:'246px'}} src={`${image.image_url_small}`} alt={name}/>
             <h2 className="text-center">{name}</h2>
 </a>
         </div>
