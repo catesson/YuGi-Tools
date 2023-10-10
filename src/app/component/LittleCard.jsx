6b9888341a08e6086322210e4f0914e59f2ef6bc
steven <steven69700@gmail.com>
2023-10-10 17:46:52 +0200

    test cypress

--- src/app/component/LittleCard.jsx
+++ src/app/component/LittleCard.jsx
@@ -1,10 +1,10 @@
-/* eslint-disable @next/next/no-img-element */
+import Image from 'next/image'
 export default function LittleCard({name, image, id}){
 
     return (
         <div className="w-52 mx-3">
             <a href={`/card/${id}`}>
-            <img className="m-auto" style={{maxWidth:'168px', maxHeight:'246px'}} src={`${image.image_url_small}`} alt={name}/>
+            <Image className="m-auto" style={{maxWidth:'168px', maxHeight:'246px'}} src={`${image.image_url_small}`} alt={name}/>
             <h2 className="text-center">{name}</h2>
 </a>
         </div>
